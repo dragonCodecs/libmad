c8733de46f04a1cbce1b1dabdb12fec5b662cb76
L. E. Segovia <amy@amyspark.me>
2023-03-24 21:49:07 +0000

    meson: Extend the symbol exports to Darwin and Unix

--- packaging/makedef.py
+++ packaging/makedef.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# This file is part of libAudio and the FFmpeg Meson build
+# This file is part of libAudio
 #
 # SPDX-FileCopyrightText: 2022-2023 amyspark <amy@amyspark.me>
 # SPDX-License-Identifier: LGPL-2.1-or-later
@@ -14,9 +14,10 @@ import subprocess
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
-        description='Craft the Windows exports file')
+        description='Craft a Windows exports or version file')
 
-    arg_parser.add_argument('--prefix', metavar='PREFIX',
+    arg_parser.add_argument('--format', metavar='FORMAT', default='windows', choices=['msvc', 'darwin', 'gcc'], help='Exports file format')
+    arg_parser.add_argument('--prefix', metavar='PREFIX', default='',
                             help='Prefix for extern symbols')
     arg_parser.add_argument('--nm', metavar='NM_PATH', type=pathlib.Path,
                             help='If specified, runs this instead of dumpbin (MinGW)')
@@ -33,8 +34,13 @@ if __name__ == '__main__':
         raise FileNotFoundError(
             errno.ENOENT, os.strerror(errno.ENOENT), libname)
 
-    prefix = args.prefix or ''
     started = 0
+
+    def removeprefix(self: str, prefix: str, /) -> str:
+        if self.startswith(prefix):
+            return self[len(prefix):]
+        else:
+            return self[:]
 
     if args.nm is not None:
         # Use eval, since NM="nm -g"
@@ -48,7 +54,7 @@ if __name__ == '__main__':
         # Take only the third field (split by spaces)
         dump = [x.split()[2] for x in dump]
         # Subst the prefix out
-        dump = [x.replace(f'^{prefix}', '') for x in dump]
+        dump = [removeprefix(x, args.prefix) for x in dump]
 
     else:
         dump = subprocess.run([args.dumpbin, '-linkermember:1', libname],
@@ -64,7 +70,7 @@ if __name__ == '__main__':
                 end = i
         dump = dump[start:end]
         # Substitute prefix out
-        dump = [re.sub(f'\s+{prefix}', ' ', x) for x in dump]
+        dump = [re.sub(f'\s+{args.prefix}', ' ', x) for x in dump]
         # Substitute big chonky spaces out
         dump = [re.sub('\s+', ' ', x) for x in dump]
         # Exclude blank lines
@@ -73,7 +79,15 @@ if __name__ == '__main__':
         # Python's split excludes whitespace at the beginning
         dump = [x.split()[1] for x in dump]
 
-    list = [f'    {i}' for i in dump]
-
-    print("EXPORTS")
-    print("\n".join(sorted(set(list))))
+    if args.format == 'msvc':
+        print("EXPORTS")
+        print("\n".join(sorted(set([f'    {i}' for i in dump]))))
+    elif args.format == 'darwin':
+        print("\n".join(sorted(set(dump))))
+    else:
+        print(f"{libname.stem.replace('.', '_')} {{")
+        print("\tglobal:")
+        print(";\n".join(sorted(set([f'\t\t{i}' for i in dump]))))
+        print("\tlocal:")
+        print("\t\t*;")
+        print("};")
