dragonCodecs/libmad

commit c8733de46f04a1cbce1b1dabdb12fec5b662cb76

meson: Extend the symbol exports to Darwin and Unix

packaging/makedef.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# This file is part of libAudio and the FFmpeg Meson build
+# This file is part of libAudio
 #
 # SPDX-FileCopyrightText: 2022-2023 amyspark <amy@amyspark.me>
 # SPDX-License-Identifier: LGPL-2.1-or-later
@@ -14,9 +14,10 @@ import subprocess
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
-        description='Craft the Windows exports file')
+        description='Craft a Windows exports or version file')
 
-    arg_parser.add_argument('--prefix', metavar='PREFIX',
+    arg_parser.add_argument('--format', metavar='FORMAT', default='windows', choices=['msvc', 'darwin', 'gcc'], help='Exports file format')
+    arg_parser.add_argument('--prefix', metavar='PREFIX', default='',
                             help='Prefix for extern symbols')
     arg_parser.add_argument('--nm', metavar='NM_PATH', type=pathlib.Path,
                             help='If specified, runs this instead of dumpbin (MinGW)')
@@ -33,8 +34,13 @@ if __name__ == '__main__':
         raise FileNotFoundError(
             errno.ENOENT, os.strerror(errno.ENOENT), libname)
 
-    prefix = args.prefix or ''
     started = 0
+
+    def removeprefix(self: str, prefix: str, /) -> str:
+        if self.startswith(prefix):
+            return self[len(prefix):]
+        else:
+            return self[:]
 
     if args.nm is not None:
         # Use eval, since NM="nm -g"
@@ -48,7 +54,7 @@ if __name__ == '__main__':
         # Take only the third field (split by spaces)
         dump = [x.split()[2] for x in dump]
         # Subst the prefix out
-        dump = [x.replace(f'^{prefix}', '') for x in dump]
+        dump = [removeprefix(x, args.prefix) for x in dump]
 
     else:
         dump = subprocess.run([args.dumpbin, '-linkermember:1', libname],
@@ -64,7 +70,7 @@ if __name__ == '__main__':
                 end = i
         dump = dump[start:end]
         # Substitute prefix out
-        dump = [re.sub(f'\s+{prefix}', ' ', x) for x in dump]
+        dump = [re.sub(f'\s+{args.prefix}', ' ', x) for x in dump]
         # Substitute big chonky spaces out
         dump = [re.sub('\s+', ' ', x) for x in dump]
         # Exclude blank lines
@@ -73,7 +79,15 @@ if __name__ == '__main__':
         # Python's split excludes whitespace at the beginning
         dump = [x.split()[1] for x in dump]
 
-    list = [f'    {i}' for i in dump]
-
-    print("EXPORTS")
-    print("\n".join(sorted(set(list))))
+    if args.format == 'msvc':
+        print("EXPORTS")
+        print("\n".join(sorted(set([f'    {i}' for i in dump]))))
+    elif args.format == 'darwin':
+        print("\n".join(sorted(set(dump))))
+    else:
+        print(f"{libname.stem.replace('.', '_')} {{")
+        print("\tglobal:")
+        print(";\n".join(sorted(set([f'\t\t{i}' for i in dump]))))
+        print("\tlocal:")
+        print("\t\t*;")
+        print("};")

meson.build
@@ -139,8 +139,15 @@ else
 endif
 
 extern_prefix = compiler.symbols_have_underscore_prefix() ? '_' : ''
-
-makedef_args += ['--prefix', extern_prefix]
+if target_machine.system() == 'windows'
+    makedef_args += ['--prefix', extern_prefix]
+    makedef_args += ['--format', 'msvc']
+elif target_machine.system() == 'darwin'
+    makedef_args += ['--format', 'darwin']
+else
+    makedef_args += ['--prefix', extern_prefix]
+    makedef_args += ['--format', 'gcc']
+endif
 
 vs_module_def = custom_target(
     'mad_def',
@@ -149,10 +156,21 @@ vs_module_def = custom_target(
     capture: true
 )
 
+mad_link_deps = []
+mad_link_args = []
+if target_machine.system() == 'darwin'
+    mad_link_args += '-Wl,-exported_symbols_list,@0@'.format(meson.current_build_dir() / 'libmad.def')
+    mad_link_deps += vs_module_def
+elif target_machine.system() != 'windows' and target_machine.system() != 'cygwin'
+    mad_link_args += '-Wl,--version-script=@0@'.format(meson.current_build_dir() / 'libmad.def')
+    mad_link_deps += vs_module_def
+endif
+
 mad = library(
     'mad',
-    c_args: mad_args,
     link_whole: mad_tmp,
+    link_args: mad_link_args,
+    link_depends: mad_link_deps,
     vs_module_defs: vs_module_def,
     version: meson.project_version(),
     soversion: meson.project_version(),

commit caf600b56850b4f88a4d6e4be9c235f60d4b1de6

meson: fix missing semicolon when generating def for GCC
Recent versions of GNU LD seems to not like missing semicolons in
version scripts definition.

This adds the missing semicolon on the last global entry and fix build
on those version of GNU LD.

Signed-off-by: Mary Guillemard <mary@mary.zone>
Fixes: c8733de46f04 ("meson: Extend the symbol exports to Darwin and Unix")

packaging/makedef.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     else:
         print(f"{libname.stem.replace('.', '_')} {{")
         print("\tglobal:")
-        print(";\n".join(sorted(set([f'\t\t{i}' for i in dump]))))
+        print(";\n".join(sorted(set([f'\t\t{i}' for i in dump]))) + ';')
         print("\tlocal:")
         print("\t\t*;")
         print("};")